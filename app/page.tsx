d66e8b10ab77f2573e90c2ca3426f9c6a1e7d651
qaeu <43359569+qaeu@users.noreply.github.com>
2024-03-09 21:38:50 +0000

    Add OptionList to main page
    With placeholder option text

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
+import OptionList from "./components/OptionList/OptionList";
 
 export default function Home() {
+  const items = ["Option 1", "Option 2", "Option 3", "Option 4"];
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="z-10 max-w-5xl font-mono text-sm">
+        <p className="my-10 border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit static w-auto rounded-xl border bg-gray-200 p-4 dark:bg-zinc-800/30">
           Placeholder text
         </p>
+        <OptionList items={items} />
       </div>
     </main>
   );
