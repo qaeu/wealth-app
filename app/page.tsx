ba101c2ded6c868b5a1e6a632f657b9edaa7ae5b
qaeu <43359569+qaeu@users.noreply.github.com>
2024-03-09 21:51:27 +0000

    Remove dark mode styling
    Will be adding support later on

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl font-mono text-sm">
-        <p className="my-10 border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit static w-auto rounded-xl border bg-gray-200 p-4 dark:bg-zinc-800/30">
+        <p className="my-10 border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl static w-auto rounded-xl border bg-gray-200 p-4">
           Placeholder text
         </p>
         <OptionList items={items} />
