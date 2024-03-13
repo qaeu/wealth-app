55c412c1c6eb81915438bfc690a8e734dc9d29b3
qaeu <43359569+qaeu@users.noreply.github.com>
2024-03-13 11:45:03 +0000

    Add previous tokens to logit_bias in ChatGPT query
    Reduces repetition in outputs

--- app/components/LLMAPI/ChatGPT.tsx
+++ app/components/LLMAPI/ChatGPT.tsx
@@ -1,33 +1,71 @@
 "use server";
 
 import OpenAI from "openai";
+import { encoding_for_model } from "tiktoken";
+
+const MODEL = "gpt-3.5-turbo";
+const IGNORE_STRINGS: string[] = ["a", "an", " of"];
+const PARAMS = {
+  maxTokens: 50,
+  frequencyPenalty: 0.2,
+  temperature: 1.1,
+  logitBiasVal: -5,
+};
 
 const openai = new OpenAI();
+const enc = encoding_for_model(MODEL);
+let previousTokens: number[] = [];
 
 export const queryChatGPT = async (prompt: string): Promise<string[]> => {
-  const decodeResponse = (response: string): string[] => {
+  const parseResponse = (response: string): string[] => {
     try {
-      const options = JSON.parse(response).items;
+      const options: string[] = JSON.parse(response).items;
+      setPreviousTokens(options);
       return options;
     } catch (error) {
       throw error;
     }
   };
 
+  const setPreviousTokens = (options: string[]): void => {
+    const tokens: number[][] = options.map((option) =>
+      Array.from(enc.encode(option))
+    );
+    previousTokens = tokens.reduce((prev, token) => [...prev, ...token]);
+
+    const ignoreStringsTokens: number[][] = IGNORE_STRINGS.map((item) =>
+      Array.from(enc.encode(item))
+    );
+    const ignoreTokens: number[] = ignoreStringsTokens.reduce((prev, token) => [
+      ...prev,
+      ...token,
+    ]);
+    previousTokens = previousTokens.filter(
+      (token) => !ignoreTokens.includes(token)
+    );
+  };
+
   try {
     const completion = await openai.chat.completions.create({
       messages: [{ role: "system", content: prompt }],
-      model: "gpt-3.5-turbo",
-      max_tokens: 50,
-      frequency_penalty: 0.2,
+      model: MODEL,
+      max_tokens: PARAMS.maxTokens,
+      frequency_penalty: PARAMS.frequencyPenalty,
       response_format: { type: "json_object" },
-      temperature: 1.1,
+      temperature: PARAMS.temperature,
+      logit_bias: previousTokens.reduce(
+        (prev, token) => ({
+          ...prev,
+          [token]: PARAMS.logitBiasVal,
+        }),
+        {}
+      ),
     });
 
     const output: string | null = completion.choices[0].message.content;
 
     if (output) {
-      return decodeResponse(output);
+      return parseResponse(output);
     } else {
       throw "blank response from ChatGPT";
     }
