1f6cbef20fe42e2fb1d2c0edb16cc86a1418b145
qaeu <43359569+qaeu@users.noreply.github.com>
2024-03-13 17:59:40 +0000

    Add performance metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children} <Analytics />
+        {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
