7c0ea4cc1aab7ad65bd1785c7fc83ebe86367ea0
qaeu <43359569+qaeu@users.noreply.github.com>
2024-03-14 13:12:42 +0000

    Add emojis to prompts

--- app/components/game/Prompts.tsx
+++ app/components/game/Prompts.tsx
@@ -1,40 +1,45 @@
 export const getOptionsPrompt = (item: string, category: string): string =>
   `Let's play a game where I trade with a variety of random items.
-Give 4 items I can trade for with (a studio mic).
-Rough category is (high-end consumer products).
+Give 4 items I can trade for with the stated item, given a rough category for creativity.
+
+item: (🎙️ a studio mic).
+category: (high-end consumer products).
 Output in JSON:
 {
     "items": [
-        "wireless headphones",
-        "a small business blog",
-        "2 bottles of aged whiskey",
-        "a cashmere scarf"
+        "🎧 wireless headphones",
+        "📰 a small business blog",
+        "🥃 2 bottles of aged whiskey",
+        "🧣 a cashmere scarf"
         ]
 }
 
-Give 4 items I can trade for with (an abandoned factory). 
-Rough category is (real estate and small business).
+item: (🏚️ an abandoned factory). 
+category: (real estate and small business).
 Output in JSON:
 {
     "items": [
-        "a classic car",
-        "an expensive highrise apartment",
-        "a local courier business",
-        "200 acres of derelict farmland"
+        "🚗 a classic car",
+        "🏙️ an expensive highrise apartment",
+        "📬 a local courier business",
+        "🌱 200 acres of derelict farmland"
         ]
 }
 
-Give 4 items I can trade for with (${item}).
-Rough category is (${category}).
+item: (${item}).
+category: (${category}).
 Output in JSON:`;
 
 export const getValuePrompt = (item: string): string =>
-  `Give a single number estimate in USD, for the value of (a jellybean):
+  `Give an estimated value in USD for the given item.
+Output only a single raw number.
+
+(🍬 a jellybean):
 0.1
-Give a single number estimate in USD, for the value of (a brand new electric car):
+(⚡ a brand new electric car):
 35000
-Give a single number estimate in USD, for the value of (an abandoned factory):
+(🏚️ an abandoned factory):
 400000
-Give a single number estimate in USD, for the value of (luxury car dealership franchise):
+(🏬 luxury car dealership franchise):
 200000000
-Give a single number estimate in USD, for the value of (${item}):`;
+(${item}):`;
