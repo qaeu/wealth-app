c3e974c673a284709facdd8552b3dab51c392a25
qaeu <43359569+qaeu@users.noreply.github.com>
2024-03-18 20:44:00 +0000

    Change page horizontal padding to half

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Game from "./components/game/Game";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-12">
       <Game />
     </main>
   );
