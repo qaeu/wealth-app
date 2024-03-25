27cf3efe9827b44ac3c09e3e559e4e503d5d6b5e
qaeu <43359569+qaeu@users.noreply.github.com>
2024-03-25 18:59:36 +0000

    Change prompts to optimise token use

--- app/components/game/Prompts.tsx
+++ app/components/game/Prompts.tsx
@@ -4,40 +4,26 @@ Offer 4 items to trade for with similar value, given a rough category for creati
 
 item: 🎙️ a studio mic
 category: high-end consumer products
-Output in JSON:
-{
-    "items": [
-        "🎧 wireless headphones",
-        "📰 a small business blog",
-        "🥃 2 bottles of aged whiskey",
-        "🧣 a cashmere scarf"
-        ]
-}
+JSON output:
+{"items": ["🎧 wireless headphones", "📰 a small business blog", "🥃 2 bottles of aged whiskey", "🧣 a cashmere scarf"]}
 
 item: 🏚️ an abandoned factory 
 category: real estate and small business
-Output in JSON:
-{
-    "items": [
-        "🚗 a classic car",
-        "🏙️ an expensive highrise apartment",
-        "📬 a local courier business",
-        "🌱 200 acres of derelict farmland"
-        ]
-}
+JSON output:
+{"items": ["🚗 a classic car", "🏙️ an expensive highrise apartment", "📬 a local courier business", "🌱 200 acres of derelict farmland"]}
 
 item: ${item}
 category: ${category}
-Output in JSON:`;
+JSON output:`;
 
 export const getValuePrompt = (item: string): string =>
   `Estimate generous upper and lower bound values in USD for the given item.
 Output only 2 raw numbers separated by a space.
 
-(⚡ 2 brand new electric cars):
+⚡ 2 brand new electric cars:
 30000 200000
-(🏚️ an abandoned factory):
+🏚️ an abandoned factory:
 200000 1000000
-(🏬 luxury car dealership franchise):
+🏬 luxury car dealership franchise:
 200000000 5000000000
-(${item}):`;
+${item}:`;
